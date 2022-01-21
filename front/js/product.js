ba8c5976b358bda13b06a95e21a73e1fb71ea443
Andy Cordon <acordon@predell.fr>
2022-01-21 13:58:42 +0100

    Etape 4 ok

---
+++ front/js/product.js
@@ -0,0 +1,38 @@
+//Etape 4 : Faire le lien entre un produit de la page d’accueil et la page Produit
+
+//Changer l'url de l'API
+const param = new URLSearchParams(document.location.search);
+let id = param.get('id');
+
+
+//Etape 5 : Récupérer l'id du produit à afficher
+
+//Appel de l'API
+const fetchProduct = async () => {
+    let fetchDataProduct = await fetch(`http://localhost:3000/api/products/${id}`);
+    let productData = await fetchDataProduct.json();
+    return productData;
+};
+
+fetchProduct();
+
+
+//Etape 6 : Insérer un produit et ses détails dans la page Produit
+
+//Insérer l'API
+const canapeViews = async () => {
+  let canapeProductView = await fetchProduct();
+  let itemImg = document.querySelector('.item__img');
+  let title = document.getElementById('title');
+  let price = document.getElementById('price');
+  let description = document.getElementById('description');
+  itemImg.innerHTML += `<img src='${canapeProductView.imageUrl}' alt=${canapeProductView.altTxt} />`;
+  title.innerHTML += `<h1 id='title'>${canapeProductView.name}</h1>`;
+  price.innerHTML += `${canapeProductView.price}`;
+  description.innerHTML += `${canapeProductView.description}`;
+};
+
+
+
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,26 @@
+//Etape 3 : Insérer les produits dans la page d’accueil
+
 //Appel de l'API
 const fetchKanap = async () => {
-  let fetchData = await fetch("http://localhost:3000/api/products");
+  let fetchData = await fetch('http://localhost:3000/api/products');
   let productKanap = await fetchData.json();
   return productKanap;
 };
 
-// Affichage des produits
-const canapeView = async () => {
-  let canapeProduct = await fetchKanap();
-  let items = document.getElementById("items");
-  for (let i = 0; i < canapeProduct.length; i += 1) {
-    items.innerHTML += `<a href="product.html?id=${canapeProduct[i]._id}"> <article> <img src="${canapeProduct[i].imageUrl}" alt="${canapeProduct[i].altTxt}" /> <h3 class="productName">${canapeProduct[i].name}</h3> <p class="productDescription">${canapeProduct[i].description}</p> </article> </a>`;
+// Affichage des elements présent dans l'API
+const kanapView = async () => {
+  let element = await fetchKanap();
+  let items = document.getElementById('items');
+  for (let i = 0; i < element.length; i++) {
+    items.innerHTML += 
+    ` <a href="product.html?id=${element[i]._id}"> 
+        <article> 
+          <img src='${element[i].imageUrl}' alt='${element[i].altTxt}' /> 
+          <h3 class='productName'>${element[i].name}</h3> 
+          <p class='productDescription'>${element[i].description}</p> 
+        </article> 
+      </a>`;
   }
 };
 
-canapeView();
+kanapView();
